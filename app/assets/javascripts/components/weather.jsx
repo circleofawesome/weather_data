7db85bd20df8a5d275e54282468b6b9afdd6d67e
Samiul Anwar <samiul.anwar@gmail.com>
2017-11-21 03:25:41 -0500

    fixed some comments

--- app/assets/javascripts/components/weather.jsx
+++ app/assets/javascripts/components/weather.jsx
@@ -1,17 +1,5 @@
 class Weather extends React.Component {
-	componentDidMount(){
-		//console.log(this.props.weather);
-		/*Object.keys(this.props.weather).map(function(index){
-			//console.log(this.props.weather[index]);
-			console.log(i);
-		});*/
-		//let arr=Object.keys(this.props.weather);
-		//console.log(typeof arr);
-		this.props.weather.map((weather)=>{
-			//console.log(weather.temp);
-		});
-	}
-
+	
   render () {
     return (<div className="table-container container">
       <table className="table is-bordered is-striped is-narrow is-fullwidth">
