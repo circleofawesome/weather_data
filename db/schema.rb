48596490b385e82f44bebcbc4a92894542e2a849
Samiul Anwar <samiul.anwar@gmail.com>
2017-11-21 11:45:57 -0500

    replaced sql with pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171121052402) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "weathers", force: :cascade do |t|
     t.datetime "date"
     t.float "temp"
